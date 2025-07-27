7169ca2fbb03a7edbae379a4a828a4ec32b3ed0c
Yanzhen Huang <yanzhenhuangwork@gmail.com>
2025-07-28 01:08:24 +0800

    feat: Add get-alive mechanism

--- src/index.ts
+++ src/index.ts
@@ -9,23 +9,54 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 
-
+/**
+ * Root api. Reserved for major use.
+ */
 app.get('/', (req: Request, res: Response) => {
     res.send("Test!!!");
 });
 
+/**
+ * GLaDoS API. Test if the server is alive.
+ */
+app.get('/are-you-still-there', (req: Request, res: Response) => {
+    res.send('still-alive');
+});
+
+/**
+ * Chat API. Send query and context, forward stream to frontend.
+ */
 app.post('/chat', async (req: Request, res: Response) => {
-    const { query } = req.body;
+    const { query, conversation_id } = req.body;
 
     const dify_url = 'https://api.dify.ai/v1/chat-messages';
     const dify_api_key = process.env.DIFY_API_KEY;
 
-    // May not use proxy
-    const proxyUrl = process.env.SYSTEM_PROXY;
-    const agent = new HttpsProxyAgent(proxyUrl || '');
-
     if (!dify_api_key) {
         return res.status(500).json({ error: 'DIFY_API_KEY not set' });
+    }
+
+    let requestConfig: any = {
+        headers: {
+            'Authorization': `Bearer ${dify_api_key}`,
+            'Content-Type': 'application/json',
+            'User-Agent': 'curl/8.0.0',
+            'Connection': 'close'
+        },
+        body: JSON.stringify({
+            inputs: {},
+            query,
+            response_mode: 'streaming',
+            conversation_id,
+            user: 'abc-123',
+        })
+    };
+
+    // May or may not use proxy
+    const proxyUrl = process.env.SYSTEM_PROXY;
+    if (proxyUrl) {
+        const agent = new HttpsProxyAgent(proxyUrl);
+        requestConfig = { agent, ...requestConfig };
     }
 
     Object.entries({
@@ -37,23 +68,7 @@ app.post('/chat', async (req: Request, res: Response) => {
     });
 
     try {
-        const res_dify = await fetch(dify_url, {
-            agent: agent,
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${dify_api_key}`,
-                'Content-Type': 'application/json',
-                'User-Agent': 'curl/8.0.0',
-                'Connection': 'close'
-            },
-            body: JSON.stringify({
-                inputs: {},
-                query: query,
-                response_mode: 'streaming',
-                conversation_id: '',
-                user: 'abc-123',
-            })
-        });
+        const res_dify = await fetch(dify_url, requestConfig);
 
         const stream = res_dify.body;
         if (!stream) {
@@ -76,12 +91,17 @@ app.post('/chat', async (req: Request, res: Response) => {
 
     } catch (e) {
         console.log(e);
-        res.status(500).json({ error: 'Failed to connect to Dify!', details: e instanceof Error ? e.message : e })
+        res.status(500).json({
+            error: 'Failed to connect to Dify!',
+            details: e instanceof Error ? e.message : e
+        })
     }
 
 });
 
-
+/**
+ * Listen to a standard port.
+ */
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
